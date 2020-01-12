991525f638cff71fbbda90013ac446ae35f4c49a
Robert Danielsson <robertdanielsson@live.com>
2020-01-12 13:09:33 +0100

    Init commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const app = express();
+const schema = require('./schema/schema')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://robert:hunter2@graphql-ninja-1ua6o.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open', () => {
+    console.log('connected to db')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
